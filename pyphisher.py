b7bbc4e61796416fa01b5d5beec8dab89edae059
Zion1TECH <157618700+Zion1TECH@users.noreply.github.com>
2024-01-25 16:43:51 +0300

    pyphisher.py

--- pyphisher.py
+++ pyphisher.py
@@ -721,7 +721,7 @@ def write_meta():
         return
     while True:
         if url is None or url == "":
-            metaurl = input(f"\n{ask}{bcyan}Enter shadow url {green}({blue}for social media preview{green}){bcyan}[{red}press enter to skip{bcyan}] : {green}")
+            metaurl = input(f"\n{ask}{bcyan}Gölge URL'sini girin {green}({blue}for social media preview{green}){bcyan}[{red}press enter to skip{bcyan}] : {green}")
         else:
             metaurl = url
         if metaurl=="":
